b786d145cf78d877cac54e879cd02a99969bdd8d
Hibanelson <76537005+Hibanelson@users.noreply.github.com>
2026-04-10 14:00:54 +0200

    Simplify next.config.ts by removing unnecessary properties
    Removed repoName, output, basePath, and assetPrefix from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "NextJSFinal";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
